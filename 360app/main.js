42fc1f84bd5c4d0ab659234f1796b8e3886c81b6
panxuesen <panxuesen@yplsec.cn>
2019-03-07 11:09:12 +0800

    下载apk

--- 360app/main.js
+++ 360app/main.js
@@ -1,3 +1,4 @@
+const http = require('http')
 const cheerio = require('cheerio')
 const request = require('request')
 const fs = require('fs')
@@ -6,7 +7,9 @@ let url = 'http://zhushou.360.cn/list/?page='
 let flag = 1 // 当前页
 let data = [] // 爬取到的数据
 let page = 10 // 爬取页数
-let total = page * 49 // 爬取总条数spider
+let total = page * 49 // 爬取总条数
+let isDownload = false // 是否下载apk
+let apkCount = 0 // apk下载数量
 
 let bar = new ProgressBar('爬取进度[:bar] :percent :elapsed秒', {
   head: '🕷',
@@ -15,10 +18,11 @@ let bar = new ProgressBar('爬取进度[:bar] :percent :elapsed秒', {
   width: 20,
   total: total // 总进度 tick调用一次默认进度加1
 })
-
+console.log('\x1B[32m%s\x1B[0m', 'Crawlers start')
+bar.tick(0)
 getHtml(flag)
 
-function getData (html) {
+function getData(html) {
   let $ = cheerio.load(html)
   let list = $('#iconList li')
   list.each(function (item) {
@@ -28,7 +32,7 @@ function getData (html) {
   })
 }
 
-function formatUrl (url) {
+function formatUrl(url) {
   let str = url.replace('zhushou360://', '')
   let arr = str.split('&')
   let data = {}
@@ -39,20 +43,45 @@ function formatUrl (url) {
   return data
 }
 
-function saveData (text) {
-  fs.appendFile(__dirname + '/data.json', text, 'utf-8', function (err) {
-    if(err){
+function saveData(data) {
+  fs.appendFile(__dirname + '/data.json', JSON.stringify(data, "", "\t"), 'utf-8', function (err) {
+    if (err) {
       console.log(err, 'err: 文件写入错误！');
+    }
+    if (isDownload) {
+      console.log('\x1B[32m%s\x1B[0m', 'Apk download start')
+      download(data, 0)
     }
   })
 }
 
-function getHtml (n) {
+function download(i) {
+  let obj = data[i]
+  http.get(obj.url, function (response) {
+    response.setEncoding('binary'); //二进制binary
+    let binaryData = '';
+    response.on('data', function (res) { //加载到内存
+      binaryData += res;
+    }).on('end', function () { //加载完
+      fs.writeFile(__dirname + '/apks/' + obj.name + '.apk', binaryData, "binary", function () {
+        apkCount ++
+        let scale = (apkCount / data.length * 100).toFixed(2) + '%'
+        console.log('\x1B[32m%s\x1B[0m', obj.name + '  ' + (binaryData.length/1024/1024).toFixed(2) + 'M  ✔  总进度' + scale)
+        data[i+1] && download(i+1)
+        if (i === data.length-1) {
+          console.log('下载完成')
+        }
+      });
+    })
+  })
+}
+
+function getHtml(n) {
   request(url + n, function (err, res, body) {
     if (!err && res.statusCode == 200) {
       getData(body)
-      flag ++;
-      if (flag > page) return saveData(JSON.stringify(data,"","\t"));
+      flag++;
+      if (flag > page) return saveData(data);
       getHtml(flag);
     } else {
       console.log('err: 请求发生错误！')
